700b3474bb20ecf37925931661a04f3a0ffac895
Timon <tka108928@iet-gibb.ch>
2018-09-20 11:00:37 +0200

    Abgabe Kompetenz A3

---
+++ M120Projekt/Layout_Aufgabe_erstellen/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Layout_Aufgabe_erstellen
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void setFaelligAm(object sender, SelectionChangedEventArgs e)
+        {
+            FaelligAmTxt.Text = datePicker.Text;
+        }
+        private void openImportantDropdown(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).ContextMenu.IsEnabled = true;
+            (sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+            (sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+            (sender as Button).ContextMenu.IsOpen = true;
+        }
+        private void changeImportantColor(Brush color)
+        {
+            ImportantColor.Background = color;
+        }
+    }
+}
